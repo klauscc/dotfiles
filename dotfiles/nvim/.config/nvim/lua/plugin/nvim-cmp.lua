c0edc3c5216d483979b69541dd767374cca32b06
chengfeng <chengfeng2333@gmail.com>
2023-03-09 15:37:17 -0500

    disable fuzzy_path

--- dotfiles/nvim/.config/nvim/lua/plugin/nvim-cmp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugin/nvim-cmp.lua
@@ -9,7 +9,7 @@ cmp.setup {
     sources = cmp.config.sources({
         {name = 'path'}, -- for cmp-path
         {name = 'ultisnips'}, -- For ultisnips users.
-        {name = 'nvim_lsp'}, -- { name = 'luasnip' }, -- For luasnip users.
+        {name = 'nvim_lsp'} -- { name = 'luasnip' }, -- For luasnip users.
         -- {
         --     name = 'fuzzy_path',
         --     options = {fd_timeout_msec = 50, fd_cmd = {'fd', '-d', '4'}}
@@ -123,7 +123,7 @@ cmp.setup {
 cmp.setup.filetype({'tex', 'markdown'}, {
     sources = cmp.config.sources({
         {name = 'buffer'}, {name = 'omni'}, {name = 'ultisnips'}, -- For ultisnips users.
-        {name = 'nvim_lsp'}
+        {name = 'nvim_lsp'}, {name = 'path'}
     })
 })
 
