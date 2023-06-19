a46f1c14f7ab03d43ed4512bb49416dd9464bd36
chengfeng <chengfeng2333@gmail.com>
2023-06-19 14:39:19 -0400

    add isort to null-ls

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -29,6 +29,7 @@ return {
           nls.builtins.formatting.stylua,
           nls.builtins.formatting.shfmt,
           nls.builtins.formatting.black,
+          nls.builtins.formatting.isort,
           -- nls.builtins.diagnostics.flake8,
         },
       }
