bda4a75ab91902347fc7b2d772281f99bcbe09e5
Feng Cheng <fncheng@meta.com>
2023-06-19 11:50:30 -0400

    update to lazyvim

---
+++ dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+
+return {{
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+    incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = ";<space>",
+      node_incremental = ";<space>",
+      scope_incremental = false,
+      node_decremental = "<bs>",
+    },
+  },
+    }
+}}
