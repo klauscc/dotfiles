fd06381c38163eefe8e0fc050f7e2e1a59e4a212
Feng Cheng <fncheng@meta.com>
2023-07-20 11:12:30 -0400

    use autosession

--- dotfiles/nvim/.config/nvim/lua/plugins/session.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/session.lua
@@ -1,15 +1,15 @@
-return {}
--- return {
---
---   { "folke/persistence.nvim", enabled = false },
---
---   {
---     "rmagatti/auto-session",
---     config = function()
---       require("auto-session").setup({
---         log_level = "error",
---         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
---       })
---     end,
---   },
--- }
+-- return {}
+return {
+
+  { "folke/persistence.nvim", enabled = false },
+
+  {
+    "rmagatti/auto-session",
+    config = function()
+      require("auto-session").setup({
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      })
+    end,
+  },
+}
