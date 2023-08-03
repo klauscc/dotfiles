0e685b1318846d305c3b42e30acaa55b7b68a4db
Feng Cheng <fncheng@meta.com>
2023-08-03 11:14:45 -0400

    disable load telescope on startup with auto-session

--- dotfiles/nvim/.config/nvim/lua/plugins/session.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/session.lua
@@ -9,6 +9,9 @@ return {
       require("auto-session").setup({
         log_level = "error",
         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+        session_lens = {
+          load_on_setup = false,
+        },
       })
     end,
   },
