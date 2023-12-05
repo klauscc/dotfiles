7bb7eda3e225fe6e1e9c5cc18545bd5b24af145f
Feng Cheng <fncheng@meta.com>
2023-12-05 18:41:23 -0500

    enable nvim-ts-context

--- dotfiles/nvim/.config/nvim/lua/plugins/disabled.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
     { "akinsho/bufferline.nvim", enabled = false },
-    { "nvim-treesitter/nvim-treesitter-context", enabled = false },
+    { "nvim-treesitter/nvim-treesitter-context", enabled = true },
     -- { "alpha-nvim",              enabled = true },
     -- {
     --     "jose-elias-alvarez/null-ls.nvim",
