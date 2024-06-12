cab13972248070555d871fe6e287b6b02f306edd
Feng Cheng <chengfeng2333@gmail.com>
2024-06-12 13:22:41 -0400

    add auto sleep unused coc instances

--- dotfiles/nvim/.config/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,28 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local api = vim.api
+
+local timeout = 30 * 60 -- timeout in seconds
+
+-- stop coc if lost focus for 5 mins
+api.nvim_create_autocmd({ "FocusLost" }, {
+  callback = function(ev)
+    _G.coc_timeout_enabled = 1
+    vim.defer_fn(function()
+      if _G.coc_timeout_enabled == 1 then
+        print("stop coc")
+        vim.cmd([[ call coc#rpc#stop()]])
+      end
+    end, 1000 * timeout)
+  end,
+})
+
+api.nvim_create_autocmd({ "FocusGained" }, {
+  callback = function(ev)
+    _G.coc_timeout_enabled = 0
+    print("start coc")
+    vim.cmd("CocStart")
+  end,
+})
