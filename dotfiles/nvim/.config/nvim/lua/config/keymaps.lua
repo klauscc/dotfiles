8259de1f57ff322ecc655aaebc7c4a8a2f0557c4
Feng Cheng <chengfeng2333@gmail.com>
2024-06-13 12:34:09 -0400

    update keymap

--- dotfiles/nvim/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,4 +7,9 @@ vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
 
 vim.keymap.set("n", ";q", ":q<CR>", { silent = true })
 vim.keymap.set("n", ";w", ":w<CR>", { silent = true })
-vim.keymap.set('v', '<leader>y', "\"+y", {noremap = true, silent = true})
+vim.keymap.set("v", "<leader>y", '"+y', { noremap = true, silent = true })
+
+-- for coc
+local opts = { silent = true, nowait = true, expr = true }
+vim.keymap.set("n", "<C-j>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-W>j"', opts)
+vim.keymap.set("n", "<C-k>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-W>k"', opts)
